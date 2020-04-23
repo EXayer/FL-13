b083084d57e7cd2e8953c8d803881c0f005e76d8
EXayer <itwasntluck@gmail.com>
2020-04-23 23:42:18 +0300

    homework #6: task 1

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,36 @@
-// Your code goes here
+(function () {
+    const check_number = Number.parseFloat(prompt('Check number'));
+
+    if (isNaN(check_number) || check_number < 0) {
+        alert('Invalid input data');
+
+        return false;
+    }
+
+    const tip_percent = Number.parseFloat(prompt('Tip'));
+
+    if (isNaN(tip_percent) || tip_percent < 0 || tip_percent > 100) {
+        alert('Invalid input data');
+
+        return false;
+    }
+
+    const tip_amount = check_number * tip_percent / 100;
+    const total = tip_amount + check_number;
+
+    alert(
+`Check number: ${check_number}
+Tip: ${tip_percent}%
+Tip amount: ${outputNumber(tip_amount)}
+Total sum to pay: ${outputNumber(total)}`
+    );
+
+    function outputNumber(number) {
+        if (number % 1 === 0) {
+            return number;
+        }
+
+        return number.toFixed(2);
+    }
+
+}());
