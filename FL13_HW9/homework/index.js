0971ea883312471392094c39d58f5174d95ef535
EXayer <itwasntluck@gmail.com>
2020-05-04 15:02:10 +0300

    homework #9: done

--- FL13_HW9/homework/index.js
+++ FL13_HW9/homework/index.js
@@ -22,8 +22,6 @@ function convert(...values) {
     return values;
 }
 
-convert('1', 2, 3, '4');
-
 /**
  * 2.
  * Iterates over array and executes function on each element.
@@ -43,8 +41,6 @@ function executeforEach(array, callback) {
         callback(array[i]);
     }
 }
-
-//executeforEach([1,2,3], function(el) {console.log(el * 2)});
 
 /**
  * 3.
@@ -70,10 +66,6 @@ function mapArray(array, callback) {
 
     return out_arr;
 }
-
-mapArray([2, '5', 8], function (el) {
-    return el + 3
-});
 
 /**
  * 4.
@@ -101,10 +93,6 @@ function filterArray(array, callback) {
     return out_arr;
 }
 
-filterArray([2, 5, 8], function(el) {
-    return el % 2 === 0
-});
-
 /**
  * 5.
  * Checks is array contains a passed value.
@@ -124,20 +112,17 @@ function containsValue(array, value) {
     let result = false;
 
     executeforEach(array, (el) => {
-        result = result || (el === value);
+        result = result || el === value;
     });
 
     return result;
 }
 
-containsValue([2, 5, 8], 2);
-containsValue([12, 4, 6], 5);
-
 /**
  * 6.
  * Reverses word.
  *
- * @param {string} word to find in.
+ * @param {string} word to reverse.
  * @returns {string}.
  * @example
  *
@@ -154,4 +139,137 @@ function flipOver(word) {
     return result;
 }
 
-flipOver('hey world');
+/**
+ * 7.
+ * Creates array from provided range.
+ *
+ * @param {array} range.
+ * @returns {array}.
+ * @example
+ *
+ * func([2, 7])
+ * // => [2, 3, 4, 5, 6, 7]
+ */
+function makeListFromRange(range) {
+    const arr = [];
+    const MAX_RANGE = 2;
+
+    if (range.length > MAX_RANGE) {
+        return arr;
+    }
+
+    for (let i = range[0]; i < range[range.length - 1] + 1; i++) {
+        arr.push(i);
+    }
+
+    return arr;
+}
+
+/**
+ * 8.
+ * Generates array of values by passed key name.
+ *
+ * @param {array} array of objects.
+ * @param {string} key to find in object.
+ * @returns {array}.
+ * @example
+ *
+ * func([
+ *  {name: 'apple', weight: 0.5},
+ *  {name: 'pineapple', weight: 2}
+ * ], 'name'))
+ * // => ["apple", "pineapple"]
+ */
+function getArrayOfKeys(array, key) {
+    const out_arr = [];
+
+    const callback = (el) => {
+        if (el.hasOwnProperty(key)) {
+            out_arr.push(el[key]);
+        }
+    };
+
+    executeforEach(array, callback);
+
+    return out_arr;
+}
+
+/**
+ * 9.
+ * Replaces numbers less then 20 and greater then 10 by '*'.
+ *
+ * @param {array} array of numbers.
+ * @returns {array}.
+ * @example
+ *
+ * func([58, 14, 48, 12, 31, 19, 10])
+ * // => [58, "*", 48, "*", 31, "*", 10]
+ */
+function substitute(array) {
+    const TOP_CAP = 20, BOTTOM_CAP = 10;
+
+    return mapArray(array, (el) => {
+        if (el < TOP_CAP && el > BOTTOM_CAP) {
+            return '*';
+        }
+
+        return el;
+    });
+}
+
+/**
+ * 10.
+ * Subtracts days from date.
+ *
+ * @param {Date} date.
+ * @param {number} days.
+ * @returns {number} day number in month.
+ * @example
+ *
+ * func(new Date(2020, 0, 2), 1)
+ * // => 1
+ *
+ * func(new Date(2020, 0, 2), 2)
+ * // => 31
+ *
+ * func(new Date(2020, 0, 2), 365)
+ * // => 2
+ */
+function getPastDay(date, days) {
+    const HOUR = 24, SECONDS = 60, MINUTES = 60, MILLISECONDS = 1000;
+    const milliseconds_back = HOUR * MINUTES * SECONDS * MILLISECONDS * days;
+    const pastDate = new Date();
+    pastDate.setTime(date.getTime() - milliseconds_back);
+
+    return pastDate.getDate();
+}
+
+/**
+ * 11.
+ * Converts date in "YYYY/MM/DD HH:mm" format.
+ *
+ * @param {Date} date.
+ * @returns {string} date in specific format.
+ * @example
+ *
+ * func(new Date('6/15/2019 09:15:00'))
+ * // => 2019/06/15 09:15
+ *
+ * func(new Date())
+ * // => 2020/05/04 14:41
+ */
+function formatDate(date) {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+
+    const MIN_TWO_SIGN = 10;
+    month = month < MIN_TWO_SIGN ? '0' + month : month;
+    day = day < MIN_TWO_SIGN ? '0' + day : day;
+    hour = hour < MIN_TWO_SIGN ? '0' + hour : hour;
+    minutes = minutes < MIN_TWO_SIGN ? '0' + minutes : minutes;
+
+    return year + '/' + month + '/' + day + ' ' + hour + ':' + minutes;
+}
